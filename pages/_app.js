510393147559d09176d02160199e7f3729f56b8a
René DeAnda <rene.ideanda@gmail.com>
2024-08-10 15:23:14 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -33,6 +33,24 @@ function MyApp({ Component, pageProps }) {
         <meta name="twitter:description" content="Stay up to date with the latest tech news from various sources in a beautiful RSS feed reader." />
         <meta name="twitter:image" content="/favicon.svg" />
         <meta name="twitter:card" content="summary_large_image" />
+      
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+          `,
+        }}
+      />
       </Head>
       <Component {...pageProps} />
     </ThemeProvider>
